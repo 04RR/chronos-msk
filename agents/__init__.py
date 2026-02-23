65982fd24b6a686df4d24b4a36ac8b2d0af452f5
Rohit Rajesh <rajesh.rohit04@gmail.com>
2026-02-23 12:55:22 +0530

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,9 +1,13 @@
 from .agent1_scout import ScoutAgent
 from .agent2_radiologist import RadiologistAgent
 from .agent3_archivist import ArchivistAgent
-from .agent4_vlm_client import LMStudioAnthropologistAgent
 from .agent5_regressor import RegressorAgent
 from .agent6_ensemble import EnsembleAgent
+
+try:
+    from .agent4_vlm_client import LMStudioAnthropologistAgent
+except ImportError:
+    LMStudioAnthropologistAgent = None
 
 __all__ = [
     "ScoutAgent",
